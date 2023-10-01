7f11dc4c20a425b6e635607735dad9044a952b73
FlamingPhoenix <flamingphoenix0013@gmail.com>
2023-10-01 16:12:02 -0400

    Update FieldCentric.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static java.lang.Math.abs;
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -18,13 +20,14 @@ import java.util.List;
 @TeleOp
 public class FieldCentric extends LinearOpMode {
 
-    private static final boolean USE_WEBCAM = false;  // true for webcam, false for phone camera
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
     DcMotor fl, fr, bl, br;
     private AprilTagProcessor aprilTag;
     DistanceSensor dl,dr;
     private VisionPortal visionPortal;
     public double motorPowerMultiplier = 1;
-    private static final int maxDistance = 20;
+    double maxDistance;
     @Override
     public void runOpMode(){
         fl = hardwareMap.dcMotor.get("fl");
@@ -37,6 +40,7 @@ public class FieldCentric extends LinearOpMode {
 
         dl = hardwareMap.get(DistanceSensor.class,"dl");
         dr = hardwareMap.get(DistanceSensor.class,"dr");
+        double baseMaxDistance = 20;
 
 
         initAprilTag();
@@ -48,27 +52,40 @@ public class FieldCentric extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()){
+            maxDistance = baseMaxDistance * Math.pow(1-gamepad1.left_stick_y,2)/3;
+            telemetry.addData("maxDistance",maxDistance);
             List<AprilTagDetection> currentDetections = aprilTag.getDetections();
             for (AprilTagDetection detection : currentDetections) {
                 if(detection.metadata!=null){
                     telemetry.addData("aprilTag","detected");
                     telemetry.addData("id",detection.id);
-                    if ((dr.getDistance(DistanceUnit.INCH) < maxDistance)||((detection.ftcPose.y < maxDistance) && (detection.id == 1 || detection.id == 2 || detection.id == 3 || detection.id == 4 || detection.id == 5 || detection.id == 6))) {
-                        motorPowerMultiplier = 0.2;
-                    } else {
+                    if ((dr.getDistance(DistanceUnit.INCH) < maxDistance)||((detection.ftcPose.y < maxDistance) && (detection.id == 1 || detection.id == 2 || detection.id == 3 || detection.id == 4 || detection.id == 5 || detection.id == 6)))
+
+                        if(gamepad1.left_stick_y>0.1){
+                            motorPowerMultiplier = 0.4;
+                        }
+                        else{
+                            motorPowerMultiplier = 0.3;
+                        } else {
                         motorPowerMultiplier = 1;
                     }
                 }
             }
             if((dr.getDistance(DistanceUnit.INCH) < maxDistance)){
-                motorPowerMultiplier = 0.2;
+
+                if(gamepad1.left_stick_y>0.1){
+                    motorPowerMultiplier = 0.4;
+                }
+                else{
+                    motorPowerMultiplier = 0.3;
+                }
             }
             else{
                 motorPowerMultiplier = 1;
             }
             telemetry.addData("distance",dr.getDistance(DistanceUnit.INCH));
             telemetry.addData("motorPower",motorPowerMultiplier);
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double y = -gamepad1.left_stick_y*motorPowerMultiplier; // Remember, this is reversed!
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
             telemetry.addData("x",x);
@@ -78,10 +95,7 @@ public class FieldCentric extends LinearOpMode {
             double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
 
 
-            if(gamepad1.left_stick_y>0){
-                motorPowerMultiplier = 0.7;
-            }
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double denominator = Math.max(abs(y) + abs(x) + abs(rx), 1);
             double flp = -(rotY + rotX + rx) / denominator;
             double blp = -(rotY - rotX + rx) / denominator;
             double frp = -(rotY - rotX - rx) / denominator;
@@ -96,12 +110,14 @@ public class FieldCentric extends LinearOpMode {
             frp = frp * (1 + 2*gamepad1.left_trigger);
             brp = brp * (1 + 2*gamepad1.left_trigger);
 
-            fl.setPower(motorPowerMultiplier*0.49*flp);
-            bl.setPower(motorPowerMultiplier*0.49*blp);
-            fr.setPower(motorPowerMultiplier*0.49*frp);
-            br.setPower(motorPowerMultiplier*0.49*brp);
+
 
             telemetry.update();
+
+            fl.setPower(0.49*flp);
+            bl.setPower(0.49*blp);
+            fr.setPower(0.49*frp);
+            br.setPower(0.49*brp);
         }
     }
     private void initAprilTag(){
@@ -116,4 +132,30 @@ public class FieldCentric extends LinearOpMode {
                     BuiltinCameraDirection.BACK, aprilTag);
         }
     }
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower    =  x -y -yaw;
+        double rightFrontPower   =  x +y +yaw;
+        double leftBackPower     =  x +y -yaw;
+        double rightBackPower    =  x -y +yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+        fl.setPower(leftFrontPower);
+        fr.setPower(rightFrontPower);
+        bl.setPower(leftBackPower);
+        br.setPower(rightBackPower);
+    }
+
 }
