e897e52f3eeaa9f8ebb597213779e3e743ce5b2c
FlamingPhoenix <flamingphoenix0013@gmail.com>
2023-11-22 17:00:44 -0500

    Update liftAnd4barServotest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/liftAnd4barServotest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/liftAnd4barServotest.java
@@ -9,14 +9,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class liftAnd4barServotest extends LinearOpMode {
     //NO LIFT YET
     DcMotor liftl,liftr, intake;
-    Servo claw;
+    Servo claw, arml, armr;
     @Override
     public void runOpMode(){
         liftl = hardwareMap.dcMotor.get("liftl");
         liftr = hardwareMap.dcMotor.get("liftr");
         intake = hardwareMap.dcMotor.get("intake");
         claw = hardwareMap.servo.get("claw");
+        //arml = hardwareMap.servo.get("arml");
+        //armr = hardwareMap.servo.get("armr");
+        //arml.setPosition(0.6);
+        //armr.setPosition(0.4);
         claw.setPosition(1);
+        liftl.setDirection(DcMotor.Direction.REVERSE);
+        liftl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //liftr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         waitForStart();
         while(opModeIsActive()) {
             if (gamepad1.a) {
@@ -27,7 +35,23 @@ public class liftAnd4barServotest extends LinearOpMode {
             }
             if(gamepad1.right_trigger > 0.1){
                 intake.setPower(gamepad1.right_trigger/2);
+            }else{
+                intake.setPower(0);
             }
+            if(gamepad1.right_stick_y>0.1){
+                liftl.setPower(gamepad1.right_stick_y/3);
+                liftr.setPower(gamepad1.right_stick_y/3);
+
+            }else if( gamepad1.right_stick_y<-0.1) {
+                liftl.setPower(gamepad1.right_stick_y/1);
+                liftr.setPower(gamepad1.right_stick_y/1);
+            }else{
+                    liftl.setPower(-0.1);
+                    liftr.setPower(-0.1);
+
+            }
+            telemetry.addData("liftr",liftr.getCurrentPosition());
+            telemetry.update();
         }
     }
 }
